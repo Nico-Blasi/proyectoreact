94e0c7c29c76abca7771bbf234be7ae335cb2c9c
Nico Blasi <nicolas.blasi.78@gmail.com>
2022-09-16 20:32:01 -0300

    push

--- src/App.jsx
+++ src/App.jsx
@@ -4,15 +4,16 @@ import NavBar from "./components/navbar/NavBar"
 import ItemListContainer from './components/itemlistcontainer/ItemListContainer';
 import ItemCount from './components/itemcount/ItemCount';
 import Item from './components/item/Item';
+import { ChakraProvider } from '@chakra-ui/react';
 
 const App = () => {
   return (
-    <>
-    <NavBar />
-    <ItemListContainer greeting="Bienvenido a mi intento de eComerce! :)" />
-    <ItemCount initial={1} stock={9} onAdd={() => {}} />
-    <Item />
-    </>
+    <ChakraProvider>
+      <NavBar />
+      <ItemListContainer greeting="Bienvenido a mi intento de eComerce! :)" />
+      <ItemCount initial={1} stock={9} onAdd={() => {}} />
+      <Item />
+    </ChakraProvider>
   )
 }
 
--- src/utils/products.js
+++ src/utils/products.js
@@ -4,41 +4,41 @@ export const products = [
       "id": 1,
       "category": "procesadores",
       "title": "Procesador",
-      "image": "https://picsum.photos/id/0/600"
+      "image": 'https://picsum.photos/id/0/600'
     },
     {
       "precio": 9000,
       "id": 2,
       "category": "memorias ram",
       "title": "RAM",
-      "image": "https://picsum.photos/id/10/600"
+      "image": 'https://picsum.photos/id/10/600'
     },
     {
       "precio": 120000,
       "id": 3,
       "category":"placas de video",
       "title": "Placa de video",
-      "image": "https://picsum.photos/id/20/600"
+      "image": 'https://picsum.photos/id/20/600'
     },
     {
       "precio": 27000,
       "id": 4,
       "category": "plcas madre",
       "title": "Placa madre",
-      "image": "https://picsum.photos/id/30/600"
+      "image": 'https://picsum.photos/id/30/600'
     },
     {
       "precio": 28000,
       "id": 5,
       "category":"monitores",
       "title": "Monitor",
-      "image": "https://picsum.photos/id/40/600"
+      "image": 'https://picsum.photos/id/40/600'
     },
     {
       "precio": 8700,
       "id": 6,
       "category":"gabinetes",
       "title": "Gabinete",
-      "image": "https://picsum.photos/id/50/600"
+      "image": 'https://picsum.photos/id/50/600'
     }
   ]
